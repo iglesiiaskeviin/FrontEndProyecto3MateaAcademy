1e58397085f827617a085f1f3a91c67971ea1589
Kevin <eiikevin@gmail.com>
2020-10-04 22:51:45 -0300

    Updates

---
+++ precache-manifest.c6f26217489b80bd46888ad3bca10c48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c0cee0a2c9b78a6549ff2993d8d2da",
+    "url": "/FrontEndProyecto3MateaAcademy/index.html"
+  },
+  {
+    "revision": "e9823e64707af0c24677",
+    "url": "/FrontEndProyecto3MateaAcademy/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d9bbfdc5711e4dbf606d",
+    "url": "/FrontEndProyecto3MateaAcademy/static/css/main.3afd5c8b.chunk.css"
+  },
+  {
+    "revision": "e9823e64707af0c24677",
+    "url": "/FrontEndProyecto3MateaAcademy/static/js/2.1c93f845.chunk.js"
+  },
+  {
+    "revision": "d6026cf0af0249d1789470681dfa69c1",
+    "url": "/FrontEndProyecto3MateaAcademy/static/js/2.1c93f845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9bbfdc5711e4dbf606d",
+    "url": "/FrontEndProyecto3MateaAcademy/static/js/main.db1618a9.chunk.js"
+  },
+  {
+    "revision": "6fa45f070dbf859518f8",
+    "url": "/FrontEndProyecto3MateaAcademy/static/js/runtime-main.3b625c27.js"
+  }
+]);
